8a8825bc05cc85ff7ce0ba5766569956a0673bb6
mikasa-akr <shaymabenamor30@gmail.com>
2023-11-20 15:59:50 +0100

    first commit

---
+++ src/test/java/com/github/projetIntegration/ProjetIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.projetIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
